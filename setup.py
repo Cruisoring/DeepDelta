d976e6bf080261bf175bdd9881032241d5524ea8
William JIANG <williamjiang0218@gmail.com>
2021-04-13 09:52:28 +1000

    import changes

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(ROOT, "README.md")) as fid:
 # This call to setup() does all the work
 setuptools.setup(
     name="deepdelta",
-    version="1.0.0",
+    version="1.0.1",
     author="William JIANG",
     author_email="williamjiang0218@gmail.com",
     description="Python comparator with rich options, configurable named or typed comparators to get deep differences "
